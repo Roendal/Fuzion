383d2db76adca89da6e7b746ae78acd668eadf9b
Eduardo Casanova <ecasanovac@gmail.com>
2011-08-19 16:38:26 +0200

    Adding some CheckRoll code

--- spec/models/tools/check_roll_spec.rb
+++ spec/models/tools/check_roll_spec.rb
@@ -1,23 +1,31 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-describe Tools::Throw do
-  
-  before do
-    @dice = [Tools::Die.new(20),Tools::Die.new(6),Tools::Die.new(20),Tools::Die.new(4),Tools::Die.new(20),
-              Tools::Die.new(100),Tools::Die.new(4),Tools::Die.new(100)]
-    @offset = 3
-    @throw = Tools::Throw.new @dice,@offset
-              
-  end  
-  
-  it "should create a Trhow" do
-    assert @throw
+
+
+describe Tools::CheckRoll do
+  describe "basic funtionality" do
+    before do
+      @dice = [Tools::Die.new(20),Tools::Die.new(6),Tools::Die.new(20),Tools::Die.new(4),Tools::Die.new(20),
+        Tools::Die.new(100),Tools::Die.new(4),Tools::Die.new(100)]
+      @offset = 3
+      @throw = Tools::Throw.new @dice,@offset
+      @threshold = 20
+
+      @check_roll = Tools::CheckRoll.new @throw, @threshold
+    end
+
+    it "should create a CheckRoll" do
+      assert @check_roll
+    end
+
+    it "should roll a CheckRoll" do
+      roll = @check_roll.roll
+      assert roll
+      assert roll[0]
+      assert roll[1]
+    end
   end
-  
-  it "should do a simple roll of a Throw once" do
-    assert @throw.roll
-  end 
-  
+
   it "should do a detailed roll of a Throw once" do
     detailed_roll = @throw.roll 1, true
     assert detailed_roll
@@ -25,17 +33,17 @@ describe Tools::Throw do
     assert detailed_roll[1]
     detailed_roll[1].each do |result|
       assert result
-    end    
-  end  
-  
+    end
+  end
+
   it "should do a simple roll of a Throw many times" do
     results = @throw.roll 30
     assert results
     results.each do |result|
       assert result
     end
-  end 
-  
+  end
+
   it "should do a detailed roll of a Throw many times" do
     detailed_rolls = @throw.roll 30, true
     assert detailed_rolls
@@ -44,12 +52,12 @@ describe Tools::Throw do
       assert detailed_roll[1]
       detailed_roll[1].each do |result|
         assert result
-      end    
+      end
     end
   end
-  
+
   it "should turn a Throw into a String" do
     assert @throw.to_s.eql? "2D4,1D6,3D20,2D100+3"
-  end  
-  
+  end
+
 end
